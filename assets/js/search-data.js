24719dadb940448589884b486ff1447c12027fd8
suhang99 <30968324+suhang99@users.noreply.github.com>
2026-08-04 08:36:43 +0000

    Deploying to gh-pages from @ suhang99/suhang99.github.io@aeada696d45a37d4736275128bbec01817495401 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -373,7 +373,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/cv.pdf", "_blank");
+          window.open("/assets/pdf/cv-en.pdf", "_blank");
         },
       },{
         id: 'social-email',
